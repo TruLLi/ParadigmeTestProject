cabd58831020ba26e7097d93bb49f2ad47d11cdd
Denis Truhlaz <d.truhlaz@immowelt.de>
2020-02-06 10:19:23 +0100

    commit

---
+++ Vjezba2.Test/BaseIntegration.cs
@@ -0,0 +1,30 @@
+﻿//using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+using Lambda3.AspNetCore.Mvc.Testing;
+
+
+namespace Vjezba2.Test
+{
+    public abstract class BaseIntegration
+    {
+        private IServiceScope scope;
+        protected IServiceProvider serviceProvider;
+        protected HttpClient client;
+
+        public static WebApplicationFactory<Startup> WebAppFactory { get; set; }
+
+        [OneTimeSetUp]
+        public void BaseIntegrationTestOneTimeSetUp()
+        {
+            client = WebAppFactory.CreateDefaultClient();
+            scope = WebAppFactory.Host.Services.CreateScope();
+            serviceProvider = scope.ServiceProvider;
+        }
+
+        [OneTimeTearDown]
+        public void BaseIntegrationTestOneTimeTearDown() => scope.Dispose();
+    }
+}
---
+++ Vjezba2.Test/WebAppFactoryExtension.cs
@@ -0,0 +1,29 @@
+﻿using Lambda3.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Vjezba2.Models;
+
+namespace Vjezba2.Test
+{
+    public static class WebAppFactoryExtension
+    {
+        public static async Task MigrateDbAndSeedAsync<TStartup>(this WebApplicationFactory<TStartup> webApplicationFactory) where TStartup : class
+        {
+            var services = webApplicationFactory.Host.Services;
+
+            using (var scope = services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<Context>();
+                var logger = scopedServices.GetRequiredService<ILogger<WebApplicationFactory<Startup>>>();
+                await db.Database.EnsureCreatedAsync();
+            }
+        }
+        public static WebApplicationFactory<TStartup> EnsureServerStarted<TStartup>(this WebApplicationFactory<TStartup> webApplicationFactory) where TStartup : class
+        {
+            webApplicationFactory.CreateDefaultClient();
+            return webApplicationFactory;
+        }
+    }
+}
