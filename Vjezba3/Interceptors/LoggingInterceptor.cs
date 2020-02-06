ff8f2843f0bb655aab734fc4b61ebf6cb860e2ba
Denis Truhlaz <d.truhlaz@immowelt.de>
2020-02-06 11:28:41 +0100

    dodavanje stopwatcha za mjerenje izvrsavanja koda

--- Vjezba3/Interceptors/LoggingInterceptor.cs
+++ Vjezba3/Interceptors/LoggingInterceptor.cs
@@ -4,29 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
+using System.Diagnostics;
+
 
 
 namespace Vjezba3.Interceptors
 {
     public class LoggingInterceptor : IInterceptor
     {
+        private Stopwatch stopwatch = new Stopwatch();
         public void Intercept(IInvocation invocation)
         {
             var methodName = invocation.Method.Name;
             try
             {
+                stopwatch.Start();
                 Console.WriteLine(string.Format("Entered Method:{0}, Arguments: {1}", methodName, string.Join(",", invocation.Arguments)));
                 invocation.Proceed();
-                Console.WriteLine(string.Format("Sucessfully executed method:{0}", methodName));
+                stopwatch.Stop();
+                Console.WriteLine(string.Format("Sucessfully executed method:{0}, duration={1}", methodName, stopwatch.Elapsed));
             }
             catch (Exception e)
             {
-                Console.WriteLine(string.Format("Method:{0}, Exception:{1}", methodName, e.Message));
+                Console.WriteLine(string.Format("Method:{0}, Exception:{1}, duration={2}", methodName, e.Message, stopwatch.Elapsed));
                 throw;
             }
             finally
             {
-                Console.WriteLine(string.Format("Exiting Method:{0}", methodName));
+                Console.WriteLine(string.Format("Exiting Method:{0}, duration={1}", methodName, stopwatch.Elapsed));
             }
         }
     }
