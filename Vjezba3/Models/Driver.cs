a3a04c6b00ea91a71d70931beef80253dd6d2666
Denis Truhlaz <d.truhlaz@immowelt.de>
2020-02-06 11:17:46 +0100

    adding logging to project

---
+++ Vjezba3/Models/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba3.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Money { get; set; }
+    }
+}
---
+++ Vjezba3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vjezba3.ComponentRegistration;
+using Vjezba3.Models;
+
+namespace Vjezba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dependency.Initialize();
+
+            var driver = Dependency.For<IDriversRepository>();
+            var newDriver = new Driver
+            {
+                Id = 1,
+                Name = "mario",
+                Surname = "zagar",
+                Money = 1000
+            };
+
+            try
+            {
+                driver.Create(newDriver);
+                driver.Get(newDriver);
+                driver.Exists(newDriver);
+                driver.Delete(newDriver);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
